c225cdc6489a08094c26e04aa84f702ce58b84ea
jacksonsuciadyy <jacksonsuciady@gmail.com>
2021-01-17 15:57:39 +0700

    initialize

---
+++ lib/helpers/db_helper.dart
@@ -0,0 +1,34 @@
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqlite_api.dart';
+
+class DBHelper {
+  static Future<Database> database() async {
+    final dbPath = await sql.getDatabasesPath();
+    return sql.openDatabase(
+      // path.join is going to find is there any DB in the 'Phone Storage' & automatically create a new DB if it didn't find any DB with the name that you've specified below
+      path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+      },
+      version: 1,
+    );
+  }
+
+  static Future<void> insert(String table, Map<String, Object> data) async {
+    // Insert Data
+    final db = await DBHelper.database();
+    db.insert(
+      table,
+      data,
+      conflictAlgorithm: sql.ConflictAlgorithm.replace,
+    );
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String table) async {
+    // Fetch Data
+    final db = await DBHelper.database();
+    return db.query(table);
+  }
+}
